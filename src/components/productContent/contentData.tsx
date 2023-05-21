0123a2e3a224e6b22b46af98b0e8c08d6903280e
Ardy Yusuf <ardyyusufw0391@gmail.com>
2023-05-21 09:02:48 +0700

    fix main bug

--- src/components/productContent/contentData.tsx
+++ src/components/productContent/contentData.tsx
@@ -4,33 +4,34 @@ interface ProductProps {
   name: string
   img: string
   price: number
+  rating: number
   nameProduct: string
   discountPrice?: number
 }
 
-// const StarIcon = ({ filled }: { filled: boolean }): JSX.Element => (
-//   <svg
-//     stroke='#DFB300'
-//     fill={filled ? '#DFB300' : '#5F5F5F'}
-//     strokeWidth='0'
-//     viewBox='0 0 576 512'
-//     height='1em'
-//     width='9.4px'
-//     xmlns='http://www.w3.org/2000/svg'>
-//     <path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path>
-//   </svg>
-// )
+const StarIcon = ({ filled }: { filled: boolean }): JSX.Element => (
+  <svg
+    stroke='#DFB300'
+    fill={filled ? '#DFB300' : '#5F5F5F'}
+    strokeWidth='0'
+    viewBox='0 0 576 512'
+    height='1em'
+    width='9.4px'
+    xmlns='http://www.w3.org/2000/svg'>
+    <path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path>
+  </svg>
+)
 
-// const Rating = ({ rating }: { rating: number }): JSX.Element => (
-//   <div className='flex gap-[2.5px]'>
-//     {Array.from(Array(5), (_, i) => (
-//       <StarIcon
-//         key={i}
-//         filled={i < rating}
-//       />
-//     ))}
-//   </div>
-// )
+const Rating = ({ rating }: { rating: number }): JSX.Element => (
+  <div className='flex gap-[2.5px]'>
+    {Array.from(Array(5), (_, i) => (
+      <StarIcon
+        key={i}
+        filled={i < rating}
+      />
+    ))}
+  </div>
+)
 
 const Price = ({
   price,
@@ -51,7 +52,7 @@ const Price = ({
   </div>
 )
 
-const Product = ({ name, img, nameProduct, price, discountPrice }: ProductProps) => {
+const Product = ({ name, img, nameProduct, rating, price, discountPrice }: ProductProps) => {
   return (
     <div className='sm:h-auto flex justify-center items-center flex-col h-[35vw] w-full'>
       <div className='items-center flex justify-center'>
@@ -66,6 +67,7 @@ const Product = ({ name, img, nameProduct, price, discountPrice }: ProductProps)
           <span className='font-normal font-inter sm:text-[12px] text-[10px] text-[#000000]/60 leading-4'>
             {name}
           </span>
+          <Rating rating={rating} />
         </div>
         <span className='font-extrabold p-2 font-inter  sm:text-[12px] text-[10px] leading-5'>
           {nameProduct}
